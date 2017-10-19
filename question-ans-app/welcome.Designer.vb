65b2350244acaf42d67939c6c345df776bf6f61e
td009 <tdtylersdavis@gmail.com>
2017-10-19 08:47:09 -0500

    add questions datagridview

--- question-ans-app/welcome.Designer.vb
+++ question-ans-app/welcome.Designer.vb
@@ -22,17 +22,59 @@ Partial Class welcome
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.dgvQuestions = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1210, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'LoadToolStripMenuItem
+        '
+        Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+        Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.LoadToolStripMenuItem.Text = "Load"
+        '
+        'dgvQuestions
+        '
+        Me.dgvQuestions.AllowUserToAddRows = False
+        Me.dgvQuestions.AllowUserToDeleteRows = False
+        Me.dgvQuestions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvQuestions.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvQuestions.Location = New System.Drawing.Point(0, 24)
+        Me.dgvQuestions.Name = "dgvQuestions"
+        Me.dgvQuestions.ReadOnly = True
+        Me.dgvQuestions.Size = New System.Drawing.Size(1210, 616)
+        Me.dgvQuestions.TabIndex = 1
         '
         'welcome
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(1210, 640)
+        Me.Controls.Add(Me.dgvQuestions)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "welcome"
         Me.Text = "Welcome"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LoadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents dgvQuestions As DataGridView
 End Class
